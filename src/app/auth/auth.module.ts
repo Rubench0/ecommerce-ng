ce95e77e316ef80973666421ba3abf4860b8b624
Ruben Garcia <rubenchoo.garcia@gmail.com>
2021-04-12 22:35:57 -0500

    formulario de login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule , FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,19 @@ import { SignupComponent } from './components/signup/signup.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule
+
+  ],
+  providers: [
+    FormBuilder
   ]
 })
 export class AuthModule { }
--- src/app/auth/components/signin/signin.component.ts
+++ src/app/auth/components/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -6,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder
+  ) { 
+    this.form = this._formBuilder.group({
+      email: [
+        '', [
+            Validators.required,
+            Validators.pattern('[A-Za-z0-9._%+-]{3,}@[a-zA-Z]{3,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})'),
+            
+          ]
+      ],
+      password: [
+        '', [
+          Validators.required,
+        ]
+      ]
+      });
+
+  }
 
   ngOnInit(): void {
   }
